1b8bde193adff9d9671b1629f4c6885e850effb1
jonghun <minn4072@gmail.com>
2023-11-28 23:10:59 +0900

    feat : interceptors, filters, validationPipe 추가

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,24 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { initializeTransactionalContext } from 'typeorm-transactional';
 import { setupSwagger } from './swagger';
+import { SuccessInterceptor } from './common/interceptors/success.interceptor';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   initializeTransactionalContext();
 
   const app = await NestFactory.create(AppModule);
   // TODO: 프로그램 구현
-
+  app.useGlobalInterceptors(new SuccessInterceptor());
+  app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      stopAtFirstError: true,
+    }),
+  );
   setupSwagger(app);
 
   await app.listen(process.env.PORT || 8000);
