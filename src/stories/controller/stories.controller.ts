2f0c8dc74f706033ed19e9607e8432abcff8ce94
jonghun <minn4072@gmail.com>
2023-12-01 00:03:53 +0900

    feat : stories 컨트롤러, 서비스 수정

--- src/stories/controller/stories.controller.ts
+++ src/stories/controller/stories.controller.ts
@@ -11,6 +11,8 @@ import { StoriesService } from '../service/stories.service';
 import { CreateStoryRequestDto } from '../dto/request.dto';
 import { ApiCreatedResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { CreateStoryResponseDto } from '../dto/response.dto';
+import { PageResponseDto } from 'src/common/dto/page.dto';
+import { ApiResponseWithDataDto } from 'src/common/dto/response.dto';
 
 @ApiTags('Story')
 @Controller('stories')
@@ -18,15 +20,18 @@ export class StoriesController {
   constructor(private readonly storiesService: StoriesService) {}
 
   @ApiOperation({ summary: '스토리 생성' })
-  @ApiCreatedResponse({ type: CreateStoryResponseDto })
+  @ApiCreatedResponse({ type: ApiResponseWithDataDto<CreateStoryResponseDto> })
   @HttpCode(HttpStatus.CREATED)
   @Post()
   create(@Body() createStoryRequestDto: CreateStoryRequestDto) {
     return this.storiesService.create(createStoryRequestDto);
   }
 
-  //pagenation
+  //pagination
   @ApiOperation({ summary: '스토리 페이지네이션 조회' })
+  @ApiCreatedResponse({
+    type: ApiResponseWithDataDto<PageResponseDto<CreateStoryResponseDto[]>>,
+  })
   @Get('/page')
   getPage(@Query('page') page: number, @Query('limit') limit: number) {
     return this.storiesService.getPage(page, limit);
--- src/stories/service/stories.service.ts
+++ src/stories/service/stories.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { CreateStoryRequestDto } from '../dto/request.dto';
 import { StoryEntity } from '../entities/story.entity';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -27,49 +27,60 @@ export class StoriesService {
       where: { userId: userId },
     });
 
-    const hashtagsArray = hashtags.map((hashtag) => {
+    // TODO: 요청한 유저가 없을 경우 => 클라이언트가 요청한 유저 id 가 없다는 것인데..
+    // 클라이언트가 요청한 유저 id 가 없다는 것은 클라이언트가 잘못 보낸다는 것? 그러므로 400 에러를 보내줘야 하나?
+    if (!user) throw new InternalServerErrorException();
+
+    const hashtagsArray = hashtags?.map((hashtag) => {
       const hashtagEntity = new HashtagEntity();
-      hashtagEntity.hashtagName = hashtag;
+      hashtagEntity.createHashtag(hashtag);
       return hashtagEntity;
     });
 
     const story = new StoryEntity();
-    story.title = title;
-    story.author = author;
-    story.image = image;
-    story.validTime = validTime;
-    story.creator = user;
-    story.hashtags = hashtagsArray;
-    story.expireAt = new Date(Date.now() + validTime * 60 * 60 * 1000);
+    story.createStory(title, author, image, validTime, hashtagsArray, user);
 
-    try {
-      const savedStory = await this.storyEntityRepository.save(story);
+    const savedStory = await this.storyEntityRepository.save(story);
+    if (!savedStory) throw new InternalServerErrorException();
 
-      const data: CreateStoryResponseDto = {
-        storyId: savedStory.storyId,
-        createdAt: savedStory.createdAt,
-        validTime: savedStory.validTime,
-        title: savedStory.title,
-        author: savedStory.author,
-        image: savedStory.image,
-        hashtags: savedStory.hashtags.map((hashtag) => hashtag.hashtagName),
-      };
+    // TODO: 이때 Mapper를 사용하면 될 것 같은데 맞을까?
+    const data: CreateStoryResponseDto = {
+      storyId: savedStory.storyId,
+      createdAt: savedStory.createdAt,
+      validTime: savedStory.validTime,
+      title: savedStory.title,
+      author: savedStory.author,
+      image: savedStory.image,
+      hashtags: savedStory.hashtags?.map((hashtag) => hashtag.hashtagName),
+    };
 
-      return data;
-    } catch (err) {
-      console.log(err);
-    }
+    return data;
   }
 
   async getPage(page: number, limit: number) {
-    const stories = await this.storyEntityRepository.find({
-      skip: (page - 1) * limit,
-      take: limit,
-      order: {
-        createdAt: 'DESC',
-      },
-    });
+    // 유효기간이 지나지 않은 스토리만 가져온다.
+    const stories = await this.storyEntityRepository
+      .createQueryBuilder('story')
+      .where('story.expireAt > :now', { now: new Date() })
+      .orderBy('story.createdAt', 'DESC')
+      .skip((page - 1) * limit)
+      .take(limit)
+      .getMany();
+    // const stories = await this.storyEntityRepository.find({
+    //   skip: (page - 1) * limit,
+    //   take: limit,
+    //   order: {
+    //     createdAt: 'DESC',
+    //   },
+    // });
 
+    // stories 총 개수를 가져온다.
+    const storiesCount = await this.storyEntityRepository
+      .createQueryBuilder('story')
+      .where('story.expireAt > :now', { now: new Date() })
+      .getCount();
+
+    // TODO: 이때 Mapper를 사용하면 될 것 같은데 맞을까?
     const content: CreateStoryResponseDto[] = stories.map((story) => ({
       storyId: story.storyId,
       createdAt: story.createdAt,
@@ -80,28 +91,14 @@ export class StoriesService {
       hashtags: story.hashtags?.map((hashtag) => hashtag.hashtagName),
     }));
 
+    // TODO: 이때 Mapper를 사용하면 될 것 같은데 맞을까?
     const data: getStoryPaginationResponseDto = {
       content: content,
       page: page,
       limit: limit,
-      totalPage: Math.ceil(stories.length / limit),
+      totalPage: Math.ceil(storiesCount / limit),
     };
 
     return data;
   }
-  // findAll() {
-  //   return `This action returns all stories`;
-  // }
-
-  // findOne(id: number) {
-  //   return `This action returns a #${id} story`;
-  // }
-
-  // update(id: number, updateStoryDto: UpdateStoryDto) {
-  //   return `This action updates a #${id} story`;
-  // }
-
-  // remove(id: number) {
-  //   return `This action removes a #${id} story`;
-  // }
 }
