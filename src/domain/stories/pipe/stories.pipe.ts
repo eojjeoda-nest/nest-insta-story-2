f5f5ea569bd4d2724a42c382cd559821f8c50b48
jiminsong490 <misosjm@naver.com>
2023-12-01 18:19:31 +0900

    refactor : custom pipe에서 hashtags를 중복없는 해쉬태그 배열로 변환

--- src/domain/stories/pipe/stories.pipe.ts
+++ src/domain/stories/pipe/stories.pipe.ts
@@ -4,16 +4,19 @@ import { BadRequestException, Injectable, PipeTransform } from '@nestjs/common';
 export class CreateStoriesPipe implements PipeTransform {
   async transform(value: any) {
     const { hashtags, validTime } = value;
-    this.toValidateHashtags(hashtags);
+    const set = new Set<string>();
+    this.toValidateHashtags(hashtags, set);
     this.toValidateValidTime(validTime);
+    this.transformHashtagsWithOutDuplicate(value, set);
     return value;
   }
   // # 확인
-  private toValidateHashtags(hashtags: string[]) {
+  private toValidateHashtags(hashtags: string[], set: Set<string>) {
     hashtags.map((hashtag) => {
       const isNumberSign = hashtag.charAt(0) === '#';
       if (!isNumberSign)
         throw new BadRequestException('해쉬태그가 #으로 시작하지 않음');
+      set.add(hashtag); // 검증된 해쉬태그를 set에 추가
     });
   }
 
@@ -21,5 +24,11 @@ export class CreateStoriesPipe implements PipeTransform {
   private toValidateValidTime(validTime: number) {
     if (validTime !== 12 && validTime !== 24)
       throw new BadRequestException('validTime에는 12와 24만 올 수 있습니다.');
+  }
+
+  // 중복된 해쉬태그 제거
+  private transformHashtagsWithOutDuplicate(value: any, set: Set<string>) {
+    value.hashtags = Array.from(set);
+    return value;
   }
 }
