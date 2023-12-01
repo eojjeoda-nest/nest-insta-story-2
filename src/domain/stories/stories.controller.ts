ab7679ba245ac30a742b596d91d1c9f6b16a3ce6
jiminsong490 <misosjm@naver.com>
2023-12-01 17:44:28 +0900

    feat : story 등록 API 구현

--- src/domain/stories/stories.controller.ts
+++ src/domain/stories/stories.controller.ts
@@ -1,7 +1,22 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Body, Controller, Post, UsePipes } from '@nestjs/common';
 import { StoriesService } from './stories.service';
+import { ApiCreatedResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { RequestStoryDto, ResponseStoryDto } from './dto/stories.dto';
+import { CreateStoriesPipe } from './pipe/stories.pipe';
 
-@Controller('stories')
+@Controller('api/v1/stories')
+@ApiTags('스토리 API')
 export class StoriesController {
   constructor(private readonly storiesService: StoriesService) {}
+
+  @Post()
+  @ApiOperation({ summary: '스토리 생성 API' })
+  @ApiCreatedResponse({
+    description: '스토리를 생성한다.',
+    type: ResponseStoryDto,
+  })
+  @UsePipes(CreateStoriesPipe) // hashtags, validTime 검증
+  createStories(@Body() req: RequestStoryDto) {
+    return this.storiesService.createStories(req);
+  }
 }
--- src/domain/stories/stories.service.ts
+++ src/domain/stories/stories.service.ts
@@ -1,6 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Story } from './entities/story.entity';
+import { RequestStoryDto } from './dto/stories.dto';
+import { StoriesMapper } from './mapper/stories.mapper';
 
 @Injectable()
 export class StoriesService {
+  constructor(
+    @InjectRepository(Story)
+    private storyRepository: Repository<Story>,
+    private storiesMapper: StoriesMapper,
+  ) {}
 
+  async createStories(req: RequestStoryDto) {
+    const story = this.storiesMapper.toEntity(req);
+    return this.storyRepository.save(story);
+  }
 }
