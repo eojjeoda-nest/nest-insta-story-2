931908bafde2b5e8cdd2c998d68d0fd103094d64
jiminsong490 <misosjm@naver.com>
2023-12-01 17:40:36 +0900

    feat : module에 mapper 추가

--- src/domain/stories/stories.module.ts
+++ src/domain/stories/stories.module.ts
@@ -3,10 +3,11 @@ import { StoriesService } from './stories.service';
 import { StoriesController } from './stories.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Story } from './entities/story.entity';
+import { StoriesMapper } from './mapper/stories.mapper';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Story])],
   controllers: [StoriesController],
-  providers: [StoriesService],
+  providers: [StoriesService, StoriesMapper],
 })
 export class StoriesModule {}
