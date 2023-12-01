d32c47bb9811d941b7d52a53994c9d34e0618f00
jiminsong490 <misosjm@naver.com>
2023-12-01 17:44:03 +0900

    chore : story dto에 swagger 명세

--- src/domain/stories/dto/stories.dto.ts
+++ src/domain/stories/dto/stories.dto.ts
@@ -1,8 +1,26 @@
+import { IsArray, IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
 export class RequestStoryDto {
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({ description: 'author' })
   readonly author: string;
-  title: string;
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({ description: 'title' })
+  readonly title: string;
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({ description: 'image' })
   readonly image: string;
+  @IsArray()
+  @IsNotEmpty()
+  @ApiProperty({ description: 'hashtags' })
   readonly hashtags: string[];
+  @IsNumber()
+  @IsNotEmpty()
+  @ApiProperty({ description: 'validTime' })
   readonly validTime: number;
 }
 
