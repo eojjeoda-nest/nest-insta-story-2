263acbc417c280d88d2d28e333e9c8406431114f
hangyeol <snake011128@naver.com>
2024-03-18 17:55:57 +0900

    feat: Swagger 설정

--- src/story/dto/request/create-story-request.dto.ts
+++ src/story/dto/request/create-story-request.dto.ts
@@ -1,19 +1,36 @@
 import { IsInt, IsString, IsUrl } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateStoryRequestDto {
 
   @IsInt()
+  @ApiProperty({
+    description: '유효시간은 12시간 또는 24시간으로 설정 가능합니다.',
+    example: 12
+  })
   validTime: number;
 
   @IsString()
+  @ApiProperty({
+    example: '어쩌다 Nest'
+  })
   title: string;
 
   @IsString()
+  @ApiProperty({
+    example: '어쩌다'
+  })
   author: string;
 
   @IsUrl()
+  @ApiProperty({
+    example: 'https://example.com/image.jpg'
+  })
   image: string;
 
   @IsString({ each: true })
+  @ApiProperty({
+    example: ['#어쩌다', '#Nest', '#당근']
+  })
   hashtags: string[];
 }
