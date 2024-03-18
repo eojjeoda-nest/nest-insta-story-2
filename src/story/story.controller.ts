e5fb294af30f96bbfee21d04dc73cee5f234a54a
hangyeol <snake011128@naver.com>
2024-03-18 17:35:26 +0900

    feat: 스토리 조회 기능 구현

--- src/story/story.controller.ts
+++ src/story/story.controller.ts
@@ -1,8 +1,9 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete, HttpCode, HttpStatus } from '@nestjs/common';
 import { StoryService } from './story.service';
 import { CreateStoryRequestDto } from './dto/request/create-story-request.dto';
+import { PaginationDto } from '../common/dto/pagination.dto';
 
-@Controller('story')
+@Controller('stories')
 export class StoryController {
   constructor(private readonly storyService: StoryService) {}
 
@@ -10,6 +11,11 @@ export class StoryController {
   @HttpCode(HttpStatus.CREATED)
   async createStory(@Body() request: CreateStoryRequestDto) {
     return await this.storyService.createStory(request);
+  }
+
+  @Get()
+  async getStories(@Body() request: PaginationDto) {
+    return await this.storyService.getStories(request);
   }
 
 }
--- src/story/story.service.ts
+++ src/story/story.service.ts
@@ -4,6 +4,8 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Story } from './entities/story.entity';
 import { Repository } from 'typeorm';
 import { CreateStoryResponseDto } from './dto/response/create-story-response.dto';
+import { PaginationDto } from '../common/dto/pagination.dto';
+import { createPaginationResult, PaginationResult } from '../common/utils/pagination.util';
 
 @Injectable()
 export class StoryService {
@@ -15,5 +17,13 @@ export class StoryService {
   async createStory(dto: CreateStoryRequestDto): Promise<CreateStoryResponseDto> {
     const entity = await this.storyRepository.save(dto);
     return CreateStoryResponseDto.fromEntity(entity);
+  }
+
+  async getStories(dto: PaginationDto): Promise<PaginationResult<Story>> {
+    const [results, total] = await this.storyRepository.findAndCount({
+      skip: (dto.page - 1) * dto.limit,
+      take: dto.limit,
+    });
+    return createPaginationResult(results, dto.page, dto.limit, total);
   }
 }
